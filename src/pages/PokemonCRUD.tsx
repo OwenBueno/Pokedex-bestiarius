11c1db69030dad5d3c598d57f4b547eaa05a5b83
Owen Sanchez <OwenMalo>
2023-12-29 19:31:11 -0600

    creation of dinamy cards for crud

--- src/pages/PokemonCRUD.tsx
+++ src/pages/PokemonCRUD.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Button, TextField, List, ListItem, ListItemText } from '@mui/material';
+import { Button, TextField, Box, ListItem, ListItemText, Card, CardActions, CardContent } from '@mui/material';
 
 
 import Header from "../components/common/Header";
@@ -12,6 +12,7 @@ interface Pokemon {
   _id: string;
   name: string;
   type: string;
+  imageUrl: string;
 }
 
 function PokemonCRUD() {
@@ -19,6 +20,7 @@ function PokemonCRUD() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const [newPokemon, setNewPokemon] = useState({ name: '', type: '' });
   const [editingPokemon, setEditingPokemon] = useState<Pokemon | null>(null);
+  const [nameError, setNameError] = useState<string>('');
 
   useEffect(() => {
     fetchData();
@@ -33,13 +35,31 @@ function PokemonCRUD() {
     }
   };
 
+  const validateInputs = (): boolean => {
+    let isValid = true;
+
+
+    if (newPokemon.name.length < 3) {
+      setNameError('Name must be at least 3 characters');
+      isValid = false;
+    } else {
+      setNameError('');
+    }
+    return isValid;
+  };
+
+
   const handleAddPokemon = async () => {
-    try {
-      await axios.post(API_URL, newPokemon);
-      setNewPokemon({ name: '', type: '' });
-      fetchData();
-    } catch (error) {
-      console.error('Error adding pokemon:', error);
+    if (validateInputs()) {
+      try {
+        const response = await axios.post(API_URL, newPokemon);
+        if(response){
+          setNewPokemon({ name: '', type: '' });
+          fetchData();
+        }
+      } catch (error) {
+        console.error('Error adding pokemon:', error);
+      }
     }
   };
 
@@ -55,12 +75,14 @@ function PokemonCRUD() {
   };
 
   const handleUpdatePokemon = async () => {
-    if (editingPokemon) {
+    if (validateInputs() && editingPokemon) {
       try {
-        await axios.put(`${API_URL}/${editingPokemon._id}`, newPokemon);
-        setEditingPokemon(null);
-        setNewPokemon({ name: '', type: '' });
-        fetchData();
+        const response = await axios.put(`${API_URL}/${editingPokemon._id}`, newPokemon);
+        if(response){
+          setEditingPokemon(null);
+          setNewPokemon({ name: '', type: '' });
+          fetchData();
+        }
       } catch (error) {
         console.error('Error updating pokemon:', error);
       }
@@ -80,66 +102,107 @@ function PokemonCRUD() {
     <div>
       <Header></Header>
       <div>
-        <h1>Pokemon CRUD</h1>
-        <List>
-          {pokemons.map((pokemon) => (
+        <h1 style={{textAlign: 'center'}}>Pokemon CRUD</h1>
+        <Box sx={{display: 'flex', flexDirection: 'column', marginLeft: '2rem'}}>
+          <Box sx={{display: 'flex', justifyContent: 'center'}}>
+            <TextField
+              sx={{margin: '5px', maxWidth: '10rem'}}
+              label="Name"
+              variant="outlined"
+              value={newPokemon.name}
+              onChange={(e) => setNewPokemon({ ...newPokemon, name: e.target.value })}
+              error={!!nameError}
+              helperText={nameError}
+            />
+            <TextField
+              sx={{margin: '5px', maxWidth: '10rem'}}
+              label="Type"
+              variant="outlined"
+              value={newPokemon.type}
+              onChange={(e) => setNewPokemon({ ...newPokemon, type: e.target.value })}
+            />
+          </Box>
+          {editingPokemon ? (
+              <Box sx={{display: 'flex', justifyContent: 'center'}}>
+                <Button sx={{margin: '5px'}} onClick={handleUpdatePokemon} variant="contained" color="primary">
+                  Update
+                </Button>
+                <Button sx={{margin: '5px'}} onClick={handleCancelEdit} variant="contained" color="warning">
+                  Cancel
+                </Button>
+              </Box>
+            ) : (
+              <Box sx={{display: 'flex', justifyContent: 'center'}}>
+                <Button sx={{margin: '5px'}} onClick={editingPokemon ? handleUpdatePokemon : handleAddPokemon} variant="contained" color="primary">
+                  {editingPokemon ? 'Update Pokemon' : 'Add Pokemon'}
+                </Button>
+              </Box>
+              )}
+        </Box>
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(178px, 1fr))', // 6 columns with a minimum width of 150px
+            gap: '16px', // Adjust the gap between items as needed
+            width: '100%',
+            maxHeight: '65vh',
+            overflowY: 'auto',
+          }}
+        >
+          {pokemons && Array.isArray(pokemons) && pokemons.map((pokemon) => (
             <ListItem key={pokemon._id}>
               {editingPokemon === pokemon ? (
-                <>
-                  <TextField
-                    label="Name"
-                    variant="outlined"
-                    value={newPokemon.name}
-                    onChange={(e) => setNewPokemon({ ...newPokemon, name: e.target.value })}
-                  />
-                  <TextField
-                    label="Type"
-                    variant="outlined"
-                    value={newPokemon.type}
-                    onChange={(e) => setNewPokemon({ ...newPokemon, type: e.target.value })}
-                  />
-                  <Button onClick={handleUpdatePokemon} variant="contained" color="primary">
-                    Update
-                  </Button>
-                  <Button onClick={handleCancelEdit} variant="contained" color="warning">
-                    Cancel
-                  </Button>
-                </>
+                <Card sx={{display:"flex", flexDirection: 'column', justifyContent:'center', padding: '1rem', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'}}>
+                  <img src={pokemon.imageUrl || '/pokemon/missing.jpeg'} alt="" />
+                  <CardContent>
+                      <ListItemText
+                          sx={{ textTransform: 'capitalize' }}
+                          primary={`${pokemon.name}`}
+                        />
+                        <ListItemText
+                          sx={{ textTransform: 'capitalize' }}
+                          primary={` - ${pokemon.type}`}
+                        />
+                    </CardContent>
+                  <CardActions>
+                    <Button onClick={handleUpdatePokemon} variant="contained" color="primary">
+                      Update
+                    </Button>
+                    <Button onClick={handleCancelEdit} variant="contained" color="warning">
+                      Cancel
+                    </Button>
+                  </CardActions>
+                  
+                </Card>
               ) : (
-                <>
-                  <ListItemText primary={`${pokemon.name} - ${pokemon.type}`} />
-                  <Button onClick={() => handleEditPokemon(pokemon)} variant="contained" color="primary">
-                    Edit
-                  </Button>
-                  <Button onClick={() => handleDeletePokemon(pokemon._id)} variant="contained" color="secondary">
-                    Delete
-                  </Button>
-                </>
+                  <Card sx={{display:"flex", flexDirection: 'column', justifyContent:'center', backgroundColor: '#f8f8ff '}}>
+                    <img src={pokemon.imageUrl || '/pokemon/missing.jpeg'} alt="" />
+                    <CardContent>
+                      <ListItemText
+                          sx={{ textTransform: 'capitalize' }}
+                          primary={`Name: ${pokemon.name}`}
+                        />
+                        <ListItemText
+                          sx={{ textTransform: 'capitalize' }}
+                          primary={`Type: ${pokemon.type}`}
+                        />
+                    </CardContent>
+                    <CardActions>
+                      <Button onClick={() => handleEditPokemon(pokemon)} variant="contained" color="primary">
+                        Edit
+                      </Button>
+                      <Button onClick={() => handleDeletePokemon(pokemon._id)} variant="contained" color="error">
+                        Delete
+                      </Button>
+                    </CardActions>
+                  </Card>
               )}
             </ListItem>
           ))}
-        </List>
-        <div>
-          <TextField
-            label="Name"
-            variant="outlined"
-            value={newPokemon.name}
-            onChange={(e) => setNewPokemon({ ...newPokemon, name: e.target.value })}
-          />
-          <TextField
-            label="Type"
-            variant="outlined"
-            value={newPokemon.type}
-            onChange={(e) => setNewPokemon({ ...newPokemon, type: e.target.value })}
-          />
-          <Button onClick={handleAddPokemon} variant="contained" color="primary">
-            Add Pokemon
-          </Button>
-        </div>
+        </Box>
       </div>
       <Footer></Footer>
     </div>
   );
 }
-
 export default PokemonCRUD;
